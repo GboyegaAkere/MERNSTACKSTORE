736fc9c8cfc00eafd5fde2aaf18018706f0bb913
Gboyega Daniel <119269023+GboyegaAkere@users.noreply.github.com>
2024-09-09 17:28:37 +0100

    worked on the login api

--- backend/controllers/userSignIn.js
+++ backend/controllers/userSignIn.js
@@ -1,5 +1,7 @@
 const userModel = require("../models/userModels")
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken")
+
 
 async function userSignInController(req,res) {
     try {
@@ -27,6 +29,24 @@ async function userSignInController(req,res) {
         //CONDITION TO SHOW IF THE PASSOWRD MATCH OR DOES NOT MATCH THE ONES IN THE DATABASE
 
         if (checkPassword) {
+            const tokenData = {
+                _id:user._id,
+                email:user.email
+            }
+
+            const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET_KEY,{expiresIn: 60 * 60 *8})
+
+            const tokenOption = {
+                httpOnly:true,
+                secure:true
+            }
+            
+            res.cookie("token",token,tokenOption).json({
+                message:"Login Successfully",
+                data:token,
+                success:true,
+                error:false
+            })
             
         } else {
             throw new Error("please check your password")
